9d408e4f92be52bc4f1bdd26673d805720bd83c9
LeviathanWang <58841009+LeviathanWang@users.noreply.github.com>
2023-08-25 15:16:40 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+import time
+
+while True:
+    current_time = time.time()
+    formatted_time = time.strftime("%H:%M:%S", time.gmtime(current_time))
+    print("Current time is:", formatted_time)
+    time.sleep(1)  # 暂停1秒
+print("time's up!")
